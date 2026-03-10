237e4694ab32c9ead90ebf2f0a1398e0209bb866
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-03-10 23:36:48 +0530

    summary.config

--- server/controllers/summary.js
+++ server/controllers/summary.js
@@ -9,3 +9,16 @@ const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: pr
       email: user.email,
       avatar: user.avatar,
       totalScans: user.totalScans,
+
+      const login = asyncHandler(async (req, res) => {
+        const { email, password } = req.body;
+      
+        // Find user and include password
+        const user = await User.findOne({ email }).select('+password');
+        if (!user) {
+          return res.status(401).json({ success: false, message: 'Invalid email or password' });
+        }
+      
+        // Check password
+        const isMatch = await user.matchPassword(password);
+        if (!isMatch) {
