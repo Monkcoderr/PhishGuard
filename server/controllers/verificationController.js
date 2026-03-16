d8e897978a9ae0f69faac74577be0d43fc575dcb
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-03-16 21:03:04 +0530

    err-sol

--- server/controllers/verificationController.js
+++ server/controllers/verificationController.js
@@ -13,3 +13,19 @@ const verification = asyncHandler(async (req, res) => {
 
     const loginVerification = asyncHandler(async (req, res) => {
       const { email, password } = req.body;
+
+
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRE });
+      
+        res.status(200).json({
+          success: true,
+          token,
+          user: {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar,
+            totalScans: user.totalScans,
+            scamsCaught: user.scamsCaught
+          }
+        });
