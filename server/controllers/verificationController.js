869ee67dfd2b41a5519d49833fc746df197b51bd
spiritualcoderr@gmail.com <spiritualcoderr@gmail.com>
2026-03-16 21:01:02 +0530

    verification userExists

---
+++ server/controllers/verificationController.js
@@ -0,0 +1,12 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+const { asyncHandler } = require('../utils/helpers');
+
+const verification = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+
+  const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ success: false, message: 'User already exists with this email' });
+    }
